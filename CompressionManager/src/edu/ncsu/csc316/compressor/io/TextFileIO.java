1df84db34b6e032bf6dce2ab042354834a572af5
jmhurst <jmhurst@ncsu.edu>
2019-10-14 13:33:09 -0400

    Added skeleton of src files

---
+++ CompressionManager/src/edu/ncsu/csc316/compressor/io/TextFileIO.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package edu.ncsu.csc316.compressor.io;
+
+import edu.ncsu.csc316.dsa.list.List;
+
+/**
+ * TextFileIO provides methods that help to read and write to given files 
+ * using Lists of strings and given file names.
+ * @author Jason Hurst
+ *
+ */
+public class TextFileIO {
+
+	/**
+	 * Returns a list of strings representing the lines of a given file.
+	 * @param fileName the file to read from
+	 * @return a list of strings representing the lines of a given file.
+	 */
+	public static List<String> readFileByLine(String fileName) {
+		return null;
+	}
+	
+	/**
+	 * Returns a list of strings representing the words of a given file.
+	 * @param fileName the file to read from
+	 * @return a list of strings representing the words of a given file.
+	 */
+	public static List<String> readFileByWord(String fileName) {
+		return null;
+	}
+	
+	/**
+	 * Writes a list of string lines to a file
+	 * @param lineList the given list of strings
+	 * @param fileName the file to write to
+	 */
+	public static void writeFile(List<String> lineList, String fileName) {
+		
+	}
+}
---
+++ CompressionManager/src/edu/ncsu/csc316/compressor/ui/CompressionManagerUI.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.ncsu.csc316.compressor.ui;
+
+/**
+ * @author Jason
+ *
+ */
+public class CompressionManagerUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
