a311b669b842756baf570f976128756d5cc6e434
gmmastra <gmmastra@ncsu.edu>
2019-10-15 01:20:45 -0400

    Adjusted delimiters to fit piazza clarification

--- CompressionManager/src/edu/ncsu/csc316/compressor/manager/CompressionManager.java
+++ CompressionManager/src/edu/ncsu/csc316/compressor/manager/CompressionManager.java
@@ -78,7 +78,7 @@ public class CompressionManager {
 
 		for (int i = 0; i < fileLines.size(); i++) {
 			Scanner wordScanner = new Scanner(fileLines.get(i));
-			wordScanner.useDelimiter(" ");
+			wordScanner.useDelimiter("[ |'|-|]");
 
 			// Gets a word from line of text
 			while (wordScanner.hasNext()) {
@@ -127,7 +127,7 @@ public class CompressionManager {
 
 		for (int i = 0; i < fileLines.size(); i++) {
 			Scanner wordScanner = new Scanner(fileLines.get(i));
-			wordScanner.useDelimiter(" ");
+			wordScanner.useDelimiter("[ |'|-|]");
 
 			// Gets a word from line of text
 			while (wordScanner.hasNext()) {
